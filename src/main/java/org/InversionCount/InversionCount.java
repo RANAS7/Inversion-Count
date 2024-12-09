9aa7486f521b78d751b63bc07415ae8f6103c020
RANAS7 <deepakrana.dr537@gmail.com>
2024-12-09 13:26:25 +0545

    ask to array size and that elements to user

--- src/main/java/org/InversionCount/InversionCount.java
+++ src/main/java/org/InversionCount/InversionCount.java
@@ -1,11 +1,24 @@
 package org.InversionCount;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class InversionCount {
     public static void main(String[] args) {
-        int[] arr = {1, 9, 6, 4, 5};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
         MergeSortInversionCounter counter = new MergeSortInversionCounter();
         int inversionCount = counter.countInversions(arr);
         System.out.println("Number of inversions: " + inversionCount);
     }
 }
-g
